510aa323624b576b506acdfc5975abd299f8691f
bkomac <b@komac.si>
2014-12-26 20:24:03 +0100

    Update Echo.java

--- src/Echo.java
+++ src/Echo.java
@@ -1,33 +1,32 @@
- package org.apache.cordova.plugin;
+package net.tracksbox.plugin.echo;
 
-    import org.apache.cordova.CordovaPlugin;
-    import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
 
-    import org.json.JSONArray;
-    import org.json.JSONException;
-    import org.json.JSONObject;
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class Echo extends CordovaPlugin {
 
-    /**
-     * This class echoes a string called from JavaScript.
-     */
-    public class Echo extends CordovaPlugin {
-
-        @Override
-        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	@Override
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
 		System.out.println("****Hallo from native...");
-            if (action.equals("echo")) {
-                String message = args.getString(0);
-                this.echo(message, callbackContext);
-                return true;
-            }
-            return false;
-        }
+		if (action.equals("echo")) {
+			String message = args.getString(0);
+			this.echo(message, callbackContext);
+			return true;
+		}
+		return false;
+	}
 
-        private void echo(String message, CallbackContext callbackContext) {
-            if (message != null && message.length() > 0) {
-                callbackContext.success("From native: "+ message);
-            } else {
-                callbackContext.error("Expected one non-empty string argument.");
-            }
-        }
-    }
+	private void echo(String message, CallbackContext callbackContext) {
+		if (message != null && message.length() > 0) {
+			callbackContext.success("From native: " + message);
+		} else {
+			callbackContext.error("Expected one non-empty string argument.");
+		}
+	}
+}
